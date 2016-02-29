e69fa751cb032fcffbabd230bc753b793a2084d1
Risjo <joris.tillet@ensta-bretagne.org>
2016-02-29 13:43:47 +0100

    Added files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 10 11:41:38 2016
+
+@author: tilletjo
+"""
+
+from segment import Segment
+from sommet import Sommet
+
+#Initialisation
+som_gauche = Sommet(0, 0)
+som_droit = Sommet(1, 0)
+h = 1
+som_milieu = Sommet(0.5, h)
+
+def main():
+    print("Projet en cours... Patientez...")
---
+++ sommet.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 10 10:23:52 2016
+
+@author: tilletjo
+"""
+
+class Sommet :
+    def __init__(self, x, h):
+        self.x = x
+        self.h = h
+    def __add__(self, som2):
+        return Sommet(self.x + som2.x, self.h + som2.h)
+    def __sub__(self, som2):
+        return Sommet(self.x - som2.x, self.h - som2.h)
+    def mul(self, c):
+        return Sommet(self.x * c, self.h * c)
+    def div(self, c):
+        assert c != 0
+        return Sommet(self.x / c, self.h / c)
+    def __str__(self):
+        return str((self.x, self.h))
+        
+        
+if __name__ == "__main__":
+    s1 = Sommet(0,0)
+    s2 = Sommet(5,-7)
+    print("s1 = " + str(s1))
+    print("s2 = " + str(s2))
+    print("s1 + s2 = " + str(s1 + s2))
+    print("s1 - s2 = " + str(s1 - s2))
+    print("s2 * 2 = " + str(s2.mul(2)))
+    print("s2 / 3 = " + str(s2.div(3)))
+    
