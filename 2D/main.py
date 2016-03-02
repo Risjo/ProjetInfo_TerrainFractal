b404c70f5a7fb368668f67e67639846bfbcdd512
Risjo <joris.tillet@ensta-bretagne.org>
2016-03-02 19:51:52 +0100

    Added files via upload

---
+++ 2D/main.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 10 11:41:38 2016
+
+@author: tilletjo
+"""
+
+from paysage import Paysage
+from sommet import Sommet
+
+#Initialisation
+som_gauche = Sommet(0, 0)
+som_droit = Sommet(1, 0)
+hauteur = 1
+som_milieu = Sommet(0.5, hauteur)
+
+paysage = Paysage(som_gauche, som_milieu, som_droit)
+
+fraction = 0.5
+
+def main():
+    nbre_coupe = input("Combien d'itération voulez-vous effectuer ? ")
+
+    for i in range(int(nbre_coupe)):
+        paysage.affine(fraction)
+    paysage.save("paysage.gnuplot")
+
+    return True
+
+main()
+print(paysage)
