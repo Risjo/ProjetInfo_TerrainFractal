784649444c0026935692f37c7302c765df346361
Risjo <joris.tillet@ensta-bretagne.org>
2016-03-13 22:05:16 +0100

    a

--- 3D matriciel/main.py
+++ 3D matriciel/main.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from paysage import Paysage
-from numpy import zeros
 
 #Initialisation
-som_gauche = (0, 0)
-som_droit = (1, 0)
-hauteur = 1
-som_milieu = (0.5, hauteur)
-
-paysage = Paysage(som_gauche, som_milieu, som_droit)
-
+paysage = Paysage([[0,0,0],[0,1,0],[0,0,0]])
 
 def main():
     nbre_coupe = ''
     while not nbre_coupe.isnumeric():
         nbre_coupe = input("Combien d'itération voulez-vous effectuer ?")
 
-    points = zeros((2**(nbre_coupe+1)+1, 2**(nbre_coupe+1)+1,))
+    for i in range(int(nbre_coupe)):
+        paysage.affine()
+
+    paysage.save("paysage3D_mat.gnuplot")
+
+    return
+
+main()
