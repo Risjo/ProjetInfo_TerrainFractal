83a04f6a5e34f0fce0d802ba3ef7083900bbcdbd
Risjo <joris.tillet@ensta-bretagne.org>
2016-03-16 17:32:24 +0100

    a

--- paysage.py
+++ paysage.py
@@ -1,8 +1,41 @@
 # -*- coding: utf-8 -*-
 
-class Paysage():
-    def __init__(self):
+from generation_terrain import *
+from creer_ciel import *
+from ajoute_cours_eau import *
+from abc import ABCMeta, abstractmethod
 
-    def creer_sommet(self):
+class Paysage(object):
+    """
+    Génération de terrain et ajout de décors (cours d'eau, ciel)
+    """
 
-    def save(self):
+    def __init__(self, dimension, nb_iteration, type_bruit, type_division):
+        """
+        Constructeur du terrain. Appelle les bonnes classes en fonction de si l'on souhaite un terrain en 2D ou en 3D,
+        du nombre d'itérations, du type de bruit et de la méthoder de division.
+        """
+        if dimension == '2D':
+            self.__terrain = Gen_2D().generate(nb_iteration, type_bruit, type_division)
+            self.__cree_ciel = Cree_2D()
+            self.__aj_cours = Ajoute_2D()
+
+        else : #dimension == '3D'
+            self.__terrain = Gen_3D().generate(type_bruit, type_division)
+            self.__cree_ciel = Cree_3D()
+            self.__aj_cours = Ajoute_3D()
+
+
+    def genere_decors(self):
+        """
+        Ajoute un ciel et un cours d'eau au terrain actuel.
+        """
+        self.__cree_ciel.creer(self.__terrain)
+        self.__aj_cours.ajoute(self.__terrain)
+
+    def __str__(self):
+        # TODO
+        """
+        :return:
+        """
+        pass
