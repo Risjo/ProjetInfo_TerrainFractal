aa529c4c9f7a40a9fb927e27bf879fe485b8fc8e
Rolletroll <roland.malleville@ensta-bretagne.org>
2016-03-17 18:13:43 +0100

    Update README.md

---
+++ 3D matriciel/main_3D_matricielle.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from Chunk import *
+from paysage_3D_matricielle import *
+
+
+
+def main():
+    paysage= Paysage(0,0,0,0)
+    print(paysage.creer_la_matrice())
+main()
--- 3D matriciel/paysage_3D_matricielle.py
+++ 3D matriciel/paysage_3D_matricielle.py
@@ -1,14 +1,13 @@
 from Chunk import *
-class paysage(object):
-    def __add__(self,sommsupg,sommsupd,somminfg,somminfd):
-        self.taille= taille
+class Paysage(object):
+    def __init__(self,sommsupg,sommsupd,somminfg,somminfd):
         self.root= Chunk(sommsupg,sommsupd,somminfg,somminfd,0)
-        self.terrain=[self.root]
-    def dévelloper(self):
+        self.terrain=[[self.root]]
+    def develloper(self):
         taille= len(self.terrain)
-        nouveauterrain= [[]*2*taille]*2*taille
+        nouveauterrain= [[]*(2*taille)]*(2*taille)
         for i in taille:
-            for j in taille):
+            for j in taille:
                 self.terrain[i][j].creerfils
                 nouveauterrain[i*2][j*2]=  self.terrain[i][j].filssupg;
                 nouveauterrain[i*2][j*2+1]= self.terrain[i][j].filssupd;
@@ -17,12 +16,17 @@ class paysage(object):
         self.terrain= nouveauterrain
     def creer_la_matrice(self):
         taille= len(self.terrain)
-        matrice= [[]*2*taille+1]*(2*taille+1)
-        for i in taille:
-            for j in taille):
+        matrice= [[]*(2*(taille+1)+1)]*(2*(taille+1)+1)
+        for i in range(taille):
+            for j in range(taille):
                 matrice[i*2][j*2]=self.terrain[i][j].sommsupg;
                 matrice[i*2][j*2+1]=self.terrain[i][j].arrsup;
                 matrice[i*2+1][j*2]= self.terrain[i][j].arrg;
                 matrice[i*2+1][j*2+1]= self.terrain[i][j].millieu;
             matrice[i*2][j*2+2]= self.terrain[i][j].sommsupd;
             matrice[i*2+1][j*2+2]= self.terrain[i][j].arrd;
+        for k in range(taille):
+            matrice[2*taille+2][taille]=self.terrain[taille][k].somminfg
+            matrice[2*taille+2][taille+1]=self.terrain[taille][k].arrinf
+        matrice[2*taille+2][2*taille+2]=self.terrain[taille][taille].somminfd
+        return matrice
