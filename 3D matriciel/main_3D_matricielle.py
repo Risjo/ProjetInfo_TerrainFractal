f546c4250f14dd3a21b2ecfc0260398cc8dde948
Risjo <joris.tillet@ensta-bretagne.org>
2016-03-17 17:39:25 +0100

    a

--- 3D matriciel/main_3D_matricielle.py
+++ 3D matriciel/main_3D_matricielle.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from Chunk import *
-from paysage_3D_matricielle import *
+
+from paysage_3D_matricielle import Paysage
 
 def main():
     paysage= Paysage(0,0,0,0)
--- 3D matriciel/paysage_3D_matricielle.py
+++ 3D matriciel/paysage_3D_matricielle.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from Chunk import Chunk
+from numpy import full
 
 class Paysage(object):
     def __init__(self,sommsupg,sommsupd,somminfg,somminfd):
-        self.root = Chunk(sommsupg,sommsupd,somminfg,somminfd,0)
+        self.root = Chunk(sommsupg,sommsupd,somminfg,somminfd,1)
         self.terrain = [[self.root]]
 
-    def develloper(self):
+    def developper(self):
         taille = len(self.terrain)
         nouveauterrain = [[]*(2*taille)]*(2*taille)
         for i in taille:
@@ -21,7 +22,7 @@ class Paysage(object):
 
     def creer_la_matrice(self):
         taille = len(self.terrain)
-        matrice = [[]*(2*(taille+1)+1)]*(2*(taille+1)+1)
+        matrice = full((2*(taille+1)+1, 2*(taille+1)+1), None)
         for i in range(taille):
             for j in range(taille):
                 matrice[i*2][j*2] = self.terrain[i][j].sommsupg
@@ -31,7 +32,7 @@ class Paysage(object):
             matrice[i*2][j*2+2] = self.terrain[i][j].sommsupd
             matrice[i*2+1][j*2+2] = self.terrain[i][j].arrd
         for k in range(taille):
-            matrice[2*taille+2][taille] = self.terrain[taille][k].somminfg
-            matrice[2*taille+2][taille+1] = self.terrain[taille][k].arrinf
-        matrice[2*taille+2][2*taille+2] = self.terrain[taille][taille].somminfd
+            matrice[2*taille+2][taille] = self.terrain[taille-1][k].somminfg
+            matrice[2*taille+2][taille+1] = self.terrain[taille-1][k].arrinf
+        matrice[2*taille+2][2*taille+2] = self.terrain[taille-1][taille-1].somminfd
         return matrice
