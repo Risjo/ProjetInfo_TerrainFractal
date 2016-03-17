8e67c63aaa3f87fa96058317c8667c62fd62e3c0
Rolletroll <roland.malleville@ensta-bretagne.org>
2016-03-17 15:25:28 +0100

    Update README.md

--- 3D matriciel/Chunk.py
+++ 3D matriciel/Chunk.py
@@ -15,6 +15,10 @@ class Chunk(object):
         self.voisinbas= None
         self.voisingauche=None
         self.voisindroite=None
+        self.filsinfg= None
+        self.filsinfd= None
+        self.filssupg= None
+        self.filssupd= None
 
     def generermillieu(self):
         bruit= (2*random.int - 1)*(1/self.taille)
@@ -22,16 +26,15 @@ class Chunk(object):
         self.millieu= bruit+moyenne
 
     def generer_arrete_g(self):
-        if self.voisingauche= None:
+        if self.voisingauche == None:
             bruit= (2*random.int - 1)*(1/self.taille)
             moyenne = (self.millieu+self.somminfg+self.sommsupg)/3
             self.arrg=bruit+moyenne
         else:
-            bruit= (2*random.int - 1)*(1/self.taille)
-            moyenne = (self.millieu+self.somminfg+self.sommsupg+self.voisingauche.milieu)/4
-            self.arrg=bruit+moyenne
+           self.arrg= self.voisingauche.arrd
+
     def generer_arrete_d(self):
-        if self.voisindroite= None:
+        if self.voisindroite == None:
             bruit= (2*random.int - 1)*(1/self.taille)
             moyenne = (self.millieu+self.somminfd+self.sommsupd)/3
             self.arrg=bruit+moyenne
@@ -39,12 +42,45 @@ class Chunk(object):
             bruit= (2*random.int - 1)*(1/self.taille)
             moyenne = (self.millieu+self.somminfg+self.sommsupg+self.voisingauche.milieu)/4
             self.arrg=bruit+moyenne
-    def generer_arrete_d(self):
-        if self.voisindroite= None:
+
+    def generer_arrete_superieure(self):
+        if self.voisinhaut == None:
             bruit= (2*random.int - 1)*(1/self.taille)
-            moyenne = (self.millieu+self.somminfd+self.sommsupd)/3
+            moyenne = (self.millieu+self.sommsupg+self.sommsupd)/3
             self.arrg=bruit+moyenne
         else:
+            self.arrsup= self.voisinhaut.arrinf
+
+    def generer_arrete_inferieure(self):
+        if self.voisinbas == None:
             bruit= (2*random.int - 1)*(1/self.taille)
-            moyenne = (self.millieu+self.somminfg+self.sommsupg+self.voisingauche.milieu)/4
+            moyenne = (self.millieu+self.sommsupg+self.sommsupd)/3
             self.arrg=bruit+moyenne
+        else:
+             bruit= (2*random.int - 1)*(1/self.taille)
+        moyenne = (self.somminfd+ self.somminfg+ self.voisinbas.milieu+self.millieu)/4
+        self.millieu= bruit+moyenne
+
+    def diviser(self):
+        self.filsinfd= Chunk(self.millieu,self.arrd,self.arrinf,self.somminfd,self.taille+1)
+        self.filsinfg= Chunk(self.arrg, self.millieu,self.somminfg,self.arrinf,self.taille+1)
+        self.filssupd= Chunk(self.arrsup, self.sommsupd,self.millieu,self.arrd,taille+1)
+        self.filssupg= Chunk(self.sommsupg, self.arrsup,self.arrg,self.millieu,taille+1)
+
+        self.filsinfd.voisingauche=self.filsinfg;
+        self.filsinfg.voisindroite= self.filsinfd;
+        self.filssupd.voisingauche=self.filssupg;
+        self.filssupg.voisindroite= self.filssupd;
+
+        self.filssupd.voisinbas=self.filsinfd
+        self.filssupg.voisinbas=self.filsinfg
+        self.filsinfd.voisinhaut=self.filssupd
+        self.filsinfg.voisinhaut=self.filssupg
+
+        if self.voisinhaut != None:
+            self.filssupg.voisinhaut =self.voisinhaut.filsinfg
+            self.voisinhaut.filsinfg.voisinbas = self.filssupg.voisinhaut
+            self.filssupd.voisinhaut =self.voisinhaut.filsinfd
+            self.voisinhaut.filsinfd.voisinbas= self.filssupd.voisinhaut
+
+        if self.voisinbas != None:
