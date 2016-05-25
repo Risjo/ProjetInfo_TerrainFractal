2082e245b8c40e3592f5cd7c998b555f1f59b654
Risjo <joris.tillet@ensta-bretagne.org>
2016-05-25 23:42:00 +0200

    a

--- NEW/cours_eau.py
+++ NEW/cours_eau.py
@@ -1,28 +1,30 @@
 # -*- coding: utf-8 -*-
 
 class CoursEau():
+
     def __init__(self, start, paysage):
         self.points = [start]
         self.paysage = paysage
 
     def coule(self):
+
         if self.points[0].est_3D :
             pass
 
         else : #2D
-            tolerance = 0.05
+            tolerance = 0.0001
             point_act = self.points[-1]
             vois1, vois2 = self.paysage.trouve_voisins(point_act)
-            if point_act.y > vois1.y and point_act.y > vois2.y:
+            if point_act.y > vois1.y - tolerance and point_act.y > vois2.y - tolerance:
                 if point_act.y - vois1.y > point_act.y - vois2.y :
                     self.points.append(vois1)
                 else:
                     self.points.append(vois2)
                 self.coule()
-            elif point_act.y > vois1.y:
+            elif point_act.y > vois1.y - tolerance:
                 self.points.append(vois1)
                 self.coule()
-            elif point_act.y > vois2.y:
+            elif point_act.y > vois2.y - tolerance:
                 self.points.append(vois2)
                 self.coule()
 
